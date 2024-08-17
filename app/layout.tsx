8403bca10cf8577a5de7a507dbeafb17f0a07c17
hkthirano <hirano.hkt@gmail.com>
2024-08-17 09:03:21 +0000

    feat: Add StairTransition component for animated page transitions

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import "./globals.css";
 // components
 import Header from "@/components/Header";
 import PageTransition from "@/components/PageTransition";
+import StairTransition from "@/components/StairTransition";
 
 const jetBrainsMono = JetBrains_Mono({
   subsets: ["latin"],
@@ -26,6 +27,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={jetBrainsMono.className}>
         <Header />
+        <StairTransition />
         <PageTransition>
           {children}
         </PageTransition>
