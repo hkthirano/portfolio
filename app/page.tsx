d48086bd337135433b65bcc69fade9c1084b7339
hkthirano <hirano.hkt@gmail.com>
2024-08-19 10:39:12 +0000

    chore: Add Photo component for displaying profile picture

--- app/page.tsx
+++ app/page.tsx
@@ -3,13 +3,14 @@ import { FiDownload } from "react-icons/fi";
 
 // components
 import Social from "@/components/Social";
+import Photo from "@/components/Photo";
 
 const Home = () => {
   return <section className="h-full">
     <div className="container mx-auto h-full">
       <div className="flex flex-col xl:flex-row items-center justify-between xl:pt-8 xl:pb-24">
         {/* text */}
-        <div className="text-center xl:text-left">
+        <div className="text-center xl:text-left order-2 xl:order-none">
           <span className="text-xl">Software Developer</span>
           <h1 className="h1 mb-6">
             Hello I&apos;m <br /> <span className="text-accent">Hokuto Hirano</span>
@@ -34,8 +35,10 @@ const Home = () => {
             </div>
           </div>
         </div>
-        {/* photon */}
-        <div>photon</div>
+        {/* photo */}
+        <div className="order-1 xl:order-none mb-8 xl:mb-0">
+          <Photo />
+        </div>
       </div>
     </div>
   </section>;
