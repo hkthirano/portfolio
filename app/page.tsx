1e0f4c0f31bc84e2b6ff7976eb7c7ae9f8a062d4
hkthirano <hirano.hkt@gmail.com>
2024-08-19 05:15:06 +0000

    refactor: Update typography styles for better readability and consistency

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,22 @@
-export default function Home() {
-  return (
-    <main>homepage</main>
-  );
-}
+const Home = () => {
+  return <section className="h-full">
+    <div className="container mx-auto h-full">
+      <div className="flex flex-col xl:flex-row items-center justify-between xl:pt-8 xl:pb-24">
+        {/* text */}
+        <div className="text-center xl:text-left">
+          <span className="text-xl">Software Developer</span>
+          <h1 className="h1 mb-6">
+            Hello I&apos;m <br /> <span className="text-accent">Hokuto Hirano</span>
+          </h1>
+          <p className="max-w-[500px] mb-9 text-white/80">
+            I excel at crafting elegant digital experiences and I am proficient in various programming languages and technologies.
+          </p>
+        </div>
+        {/* photon */}
+        <div>photon</div>
+      </div>
+    </div>
+  </section>;
+};
+
+export default Home;
