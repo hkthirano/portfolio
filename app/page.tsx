b8f8555b2db24c92b7b7f87c66de79bda4439b64
hkthirano <hirano.hkt@gmail.com>
2024-08-19 13:58:46 +0000

    feat: Add Stats component for displaying various statistics on the Home page

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import { FiDownload } from "react-icons/fi";
 // components
 import Social from "@/components/Social";
 import Photo from "@/components/Photo";
+import Stats from "@/components/Stats";
 
 const Home = () => {
   return <section className="h-full">
@@ -41,6 +42,7 @@ const Home = () => {
         </div>
       </div>
     </div>
+    <Stats />
   </section>;
 };
 
